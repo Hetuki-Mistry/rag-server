7141aef8418801dc603a85f84ecb2d57feeaba60
Hetuki <hetukim.itpath@gmail.com>
2025-07-02 15:46:53 +0530

    updated uvicorn name

--- rag_server.py
+++ rag_server.py
@@ -147,4 +147,4 @@ def rag_tool(query: str) -> str:
 app = mcp.streamable_http_app
 
 if __name__ == "__main__":
-    uvicorn.run(mcp.streamable_http_app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
